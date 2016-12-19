c9e74580707c0509026f6bf0501fa3d4509efbac
fxlik <felik.official@gmail.com>
2016-12-19 22:01:08 +0700

    activation user added

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,6 +28,7 @@ Route::get('/', function () {
 
 Route::group(['middleware' => ['web']], function () {
 	Route::auth();
+	Route::get('user/activation/{token}', 'Auth\AuthController@activateUser')->name('user.activate');
     //Login Routes...
     Route::get('/admin2/login','AdminAuth\AuthController@showLoginForm');
     Route::post('/admin/login','AdminAuth\AuthController@login');
